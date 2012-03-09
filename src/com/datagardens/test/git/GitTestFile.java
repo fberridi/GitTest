22058396e307e4995fe0af1322dbe2d5588fb288
Francisco Berridi <fberridi@datagardens.com>
2012-03-09 12:34:07 -0700

    Second project commit

--- src/com/datagardens/test/git/GitTestFile.java
+++ src/com/datagardens/test/git/GitTestFile.java
@@ -2,4 +2,29 @@ package com.datagardens.test.git;
 
 public class GitTestFile {
 
+	private String id;
+	private String name;
+	
+	public GitTestFile(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
